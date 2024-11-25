8d343ce50d4f30091e92ba8b3ca4fddfbe1e7ce3
a-kc <62874168+a-kc@users.noreply.github.com>
2024-11-25 17:58:25 +0530

    Initial Commit

---
+++ Test-ios/Test_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_iosApp.swift
+//  Test-ios
+//
+//  Created by Aniket Chauhan on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
